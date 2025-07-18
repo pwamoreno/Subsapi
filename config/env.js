ac066736ffe336972c9dbb43b5a03b6e52aa53d0
bckspce <49206472+pwamoreno@users.noreply.github.com>
2025-07-18 20:33:48 +0100

    mailing and nodemailer

--- config/env.js
+++ config/env.js
@@ -12,5 +12,6 @@ export const {
   ARCJET_ENV,
   QSTASH_TOKEN,
   QSTASH_URL,
-  SERVER_URL
+  SERVER_URL,
+  EMAIL_PASSWORD
 } = process.env;
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import Subscription from "../models/subscription.model.js";
 
 import { createRequire } from "module";
+import { sendReminderEmail } from "../utils/send-emails.js";
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 
@@ -33,7 +34,7 @@ export const sendReminders = serve(async (context) => {
       );
     }
 
-    await triggerReminder(context, `Reminder ${daysBefore} days before`);
+    await triggerReminder(context, `${daysBefore} days before reminder`, subscription);
   }
 });
 
@@ -48,8 +49,14 @@ const sleepUntilReminder = async (context, label, date) => {
   await context.sleepUntil(label, date.toDate());
 };
 
-const triggerReminder = async (context, label) => {
-  return await context.run(label, () => {
-    console.log(`Triggering ${label} reminder`);
+const triggerReminder = async (context, label, subscription) => {
+  return await context.run(label, async () => {
+    console.log(`Triggering ${label}`);
+
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: label,
+      subscription,
+    });
   });
 };
